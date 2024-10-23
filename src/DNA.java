fdadc6a3ea2a47b553a6512146045f9e4c852d55
lmoceri723 <landon.moceri@menloschool.org>
2024-10-23 09:03:22 -0700

    started hashing

--- src/DNA.java
+++ src/DNA.java
@@ -11,77 +11,42 @@
  */
 
 public class DNA {
+    public static int radix = 256;
+    public static long largePrime = 54321102419L;
+
+    public static long hash(String string, int length) {
+        long hash = 0;
+        for (int i = 0; i < length; i++)
+            hash = (hash * radix + string.charAt(i)) % largePrime;
+        return hash;
+    }
+
+    public static void rabinKarp(String STR, String sequence) {
+        long strHash = hash(STR, STR.length());
+        int strLength = STR.length();
+        long seqHash = hash(sequence.substring(0, strLength), strLength);
+        long seqLength = sequence.length();
+
+        for (int i = strLength; i < seqLength; i++)
+            if (strHash == seqHash)
+                // begin checking for consecutive appearances
+                seqHash = hash(sequence[1 + (i - strLength), i])
+
+
+    }
+
+
 
     public static int STRCount(String sequence, String STR) {
 
         // Create a map from the characters to their integer values
-        char[] charArray = sequence.toCharArray();
-        byte[] map = new byte[256];
-        map['A'] = 0;
-        map['C'] = 1;
-        map['G'] = 2;
-        map['T'] = 3;
+       char[] charArray = sequence.toCharArray();
+//        byte[] map = new byte[256];
+//        map['A'] = 0;
+//        map['C'] = 1;
+//        map['G'] = 2;
+//        map['T'] = 3;
 
-        // Transform the STR into an integer
-        int transformedSTR = 0;
-        int strLength = STR.length();
-        for (int i = 0; i < strLength; i++) {
-            int code = map[STR.charAt(i)];
-            transformedSTR = transformedSTR << 2;
-            transformedSTR = transformedSTR | code;
-        }
 
-        int highestCount = 0;
-        int buffer = 0;
-
-        // Fill the buffer with the first str.length characters
-        for (int i = 0; i < strLength; i++) {
-            buffer = buffer << 2;
-            buffer = buffer | map[charArray[i]];
-        }
-
-        // Make a mask to clear the highest (32 - 2 * STR.length) bits of the buffer
-        int mask = (1 << (2 * strLength)) - 1;
-
-        // Iterate through the rest of the sequence, updating the buffer and checking for matches
-        int index = strLength;
-        int count = 0;
-        while (index <= charArray.length) {
-            // Clear the highest bits of the buffer, as we only care about the last 2 * STR.length bits
-            buffer = buffer & mask;
-
-            // Check if the buffer matches the transformed STR
-            if (buffer == transformedSTR) {
-                count++;
-                // Move ahead by the length of the STR, as we have found a match
-                index += strLength;
-
-                // Update the buffer with the next STR.length characters
-                // If index is higher than the length of the sequence, do not update the buffer as the STR can't exist
-                if (index <= charArray.length) {
-                    buffer = 0;
-                    for (int i = 0; i < strLength; i++) {
-                        buffer = buffer << 2;
-                        buffer = buffer | map[charArray[index - strLength + i]];
-                    }
-                }
-                // If the buffer does not match the transformed STR, move to the next character
-            } else {
-                if (index < charArray.length) {
-                    buffer = buffer << 2;
-                    buffer = buffer | map[charArray[index]];
-                }
-                index++;
-                // If the buffer is not a match, reset the count
-                count = 0;
-            }
-
-            // Update the highest count
-            if (count > highestCount) {
-                highestCount = count;
-            }
-        }
-
-        return highestCount;
     }
 }
